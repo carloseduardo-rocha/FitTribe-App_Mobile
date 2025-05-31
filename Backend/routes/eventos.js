215439b43d7e5b7bc3e0b8922108bf764062a59a
cadu321r <cadurocha39@gmail.com>
2025-05-31 16:43:31 -0300

    Adicionemnto de nova funcionalidades

--- Backend/routes/eventos.js
+++ Backend/routes/eventos.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const eventos = require('../data/eventos.json');
+const buscarEventosCorreCeara = require('../utils/scrapeEventos');
 
 router.get('/', (req, res) => {
   res.json(eventos);
+});
+
+router.get('/externos', async (req, res) => {
+  try {
+    const eventosExternos = await buscarEventosCorreCeara();
+    res.json(eventosExternos);
+  } catch (err) {
+    console.error('Erro ao buscar eventos externos:', err);
+    res.status(500).json({ erro: 'Erro ao buscar eventos externos' });
+  }
 });
 
 module.exports = router;
